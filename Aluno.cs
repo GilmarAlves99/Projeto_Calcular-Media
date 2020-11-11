7101206fed70c98e30b9b82ab019640cc6a4872a
Gilmar Zezilia Alves <68366424+GilmarAlves99@users.noreply.github.com>
2020-11-11 20:49:58 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularMedia
+{
+    class Aluno
+    {
+
+ private string nome;
+        private double nota1;
+        private double nota2;
+        private double media;
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setNota1(double nota1)
+        {
+            this.nota1 = nota1;
+        }
+        public double getNota1()
+        {
+            return nota1;
+        }
+        public void setNota2(double nota2)
+        {
+            this.nota2 = nota2;
+        }
+        public double getNota2()
+        {
+            return nota2;
+        }
+        public void setMedia(double media)
+        {
+            this.media = media;
+        }
+        public double getMedia()
+        {
+            return media;
+        }
+        public void calcularMedia()
+        {
+            media = ((nota1 + nota2) / 2);
+        }
+        public void verificarSituacao()
+        {
+            if (media < 3)
+            {
+                MessageBox.Show("Aluno " + nome + " : Reprovado \n Média : " + media);
+            }
+            if ((media >= 3) && (media <= 6))
+            {
+                MessageBox.Show("Aluno " + nome + " : em Exame \n Média : " + media);
+            }
+            if (media > 6)
+            {
+                MessageBox.Show("Aluno " + nome + " : Aprovado \n Média : " + media);
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Aluno alu = new Aluno();
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+            alu.setNome(txtnome.Text);
+            alu.setNota1(double.Parse(txtnota1.Text));
+            alu.setNota2(double.Parse(txtnota2.Text));
+            MessageBox.Show("Aluno Cadastrado");
+        }
+
+      
+
+        private void btnlistar_Click_1(object sender, EventArgs e)
+        {
+            alu.calcularMedia();
+            txtmedia.Text = Convert.ToString(alu.getMedia());
+            txtnota1.Text = Convert.ToString(alu.getNota1());
+            txtnota2.Text = Convert.ToString(alu.getNota2());
+            txtnome.Text = alu.getNome();
+            alu.verificarSituacao();
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtnome.Text = "";
+            txtnota1.Text = "";
+            txtnota2.Text = "";
+            txtmedia.Text = "";
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
